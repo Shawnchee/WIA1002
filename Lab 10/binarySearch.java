abf9506584a91ebffe4eaa90987086be4302180f
Shawnchee <aykaychee2@gmail.com>
2023-06-24 00:08:00 +0800

    Binary search uploaded 😃

--- Lab 10/binarySearch.java
+++ Lab 10/binarySearch.java
@@ -1,3 +1,22 @@
 public class binarySearch {
-    
+    public static void main(String[] args) {
+        int [] array = {2,3,6,7,1,5,9,8};
+        int target = 9;
+        int result = BinarySearch(array, target);
+        System.out.println(target+" is found at index: " + result);
+    }
+
+    public static int BinarySearch(int[] array, int target) {
+        int low  = 0, high = array.length-1 ;
+
+        while (low <= high){
+            int middle = (low+high)/2;
+            if (array[middle] == target) return middle;
+
+            else if (array[middle]>target)  high = middle -1;
+
+            else  low = middle + 1;
+        }
+        return -1 ;
+    }
 }
