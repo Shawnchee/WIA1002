4f25100b3bd8d4751934cc9af6e55e9721dd30fd
Shawnchee <aykaychee2@gmail.com>
2023-07-05 00:20:36 +0800

    Updated Lab11

--- Lab 11/BST.java
+++ Lab 11/BST.java
@@ -22,9 +22,51 @@ public class BST<E> extends Comparable<E> {
         if (root==null)
         root = new TreeNode(e);
         // code below is for finding the parent node
+        TreeNode<E> current = root;
+        TreeNode<E> parent;
+        while (current != null){
+            if (e < current.e){
+                parent = current;
+                current = current.left;
+            }
+            else if (e > current.e){
+                parent = current;
+                current = current.right;
+            }
+            else return false;      //duplicated node is not inserted
+        }
+        //creating a new node and attaching it to the current node;
+        if (e < parent.e){
+             parent.left = new TreeNode(e);
+        }
+        else if (e > parent.e){
+            parent.right = new TreeNode(e);
+        }
+            size++;
+            return true;
+    }
 
+    public int getSize(){
+        return size;
+    }
 
+    public int height(){
+        return height(root);
+    }
+    private int height(TreeNode<E> node){
+        int height = -1;
+        TreeNode<E> current = root;
+        while (current!=null){
+            height++;
+            if (current.compareTo(node) == 0) break;
 
+            current = node.compareTo(current) < 0 ? current.left : current.right;
+        }
+
+        return height;
+    }
+    public E getRoot(){
+        return root.element;
     }
 }
 
